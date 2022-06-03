4a0855bba667496f9052eb2887bf38a27fae79c4
Izul Wahidin <izul@wahid.in>
2022-06-03 18:24:50 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 envsubst < /etc/caddy/Caddyfile > /etc/caddy/Caddyfile
 caddy start
 cat /etc/caddy/Caddyfile
-echo "[WN] Wahidin\n Proxing from ${host} to port ${PORT}"
+echo "[WN] Wahidin - Proxing from ${FROM} to port ${PORT} / ${DOMAIN}"
